95c8c01f025135e84dbefc19dfeac5d58e5ec944
KimP0SSIB1E <165786987+KimP0SSIB1E@users.noreply.github.com>
2024-09-25 10:52:35 +0100

    The sevices of the project worked on

---
+++ IdeaProjects/araha/src/main/java/com/semicolon/africa/dtos/request/SubscriptionRequest.java
@@ -0,0 +1,11 @@
+package com.semicolon.africa.dtos.request;
+
+import com.semicolon.africa.constants.SubscriptionType;
+
+public class SubscriptionRequest {
+
+    private SubscriptionType subscriptionType;
+    private double amount;
+    private String description;
+
+}
